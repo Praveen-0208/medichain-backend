b3d80de3613b8464c99e9e8225e396ae781a4935
Olagammal Sivasubramanyan <osolagammal@gmail.com>
2022-04-01 16:51:08 +0530

    setup user contract

--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -17,9 +17,18 @@ async function main() {
   const Greeter = await hre.ethers.getContractFactory("Greeter");
   const greeter = await Greeter.deploy("Hello, Hardhat!");
 
+  const UserContract = await hre.ethers.getContractFactory("UserContract");
+  const userContract = await UserContract.deploy();
+
+
+
   await greeter.deployed();
+  await userContract.deployed();
 
   console.log("Greeter deployed to:", greeter.address);
+  console.log("UserContract deployed to:", userContract.address);
+  // const result = await userContract.addUser('0x452cD9df789D706f01b0DD5835a081d0E92825F1', 'abc', 'abc')
+  console.log(result, "is the result")
 }
 
 // We recommend this pattern to be able to use async/await everywhere
